5202f1ab40bb409a7d5abb180cc45050cb116eed
Jorrit Poelen <jhpoelen@xs4all.nl>
2019-01-21 11:12:08 -0800

    retain previous 200s  in checklist_status to avoid confusion

--- checklist_script_gen.sh
+++ checklist_script_gen.sh
@@ -8,13 +8,16 @@ generate_status_script() {
   if [ -f checklist_status.tsv ]
   then
     # only check geonames_id without prior 200 status code
+    cat checklist_status.tsv > checklist_status_previous.tsv
     cat checklist_status.tsv | tail -n +2 | grep -v "200$" | cut -f1 | sort -n > geonames_id_non_200.tsv
     cat wkt_string.tsv | sort -n > wkt_string_sorted.tsv
     join --nocheck-order wkt_string_sorted.tsv geonames_id_non_200.tsv > wkt_string_to_be_checked.tsv -t $'\t'
+    cat checklist_status_previous.tsv | head -n1 > checklist_status.tsv
+    cat checklist_status_previous.tsv | tail -n +2 | grep "200$" >> checklist_status.tsv
   else
     cat wkt_string.tsv > wkt_string_to_be_checked.tsv
+    echo "echo \"geonames_id\\\tchecked_at\\\thttp_status_code\" > checklist_status.tsv" > checklist_status.sh
   fi
-  echo "echo \"geonames_id\\\tchecked_at\\\thttp_status_code\" > checklist_status.tsv" > checklist_status.sh
   cat wkt_string_to_be_checked.tsv | awk -F '\t'  '{ print "curl -s -o /dev/null -w \"%{http_code}\" \"http://api.effechecka.org/checklist.tsv?limit=1&wktString=" $2 "\"| xargs echo " $1 "\\\t$(date -Is)\\\t >> checklist_status.tsv"; }' >> checklist_status.sh
 }
 
