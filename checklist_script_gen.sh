70cde04ba8da41307332f525fa185471c99c2e99
jhpoelen <jhpoelen@xs4all.nl>
2019-01-25 10:56:22 -0800

    attempt to fix checklist script

--- checklist_script_gen.sh
+++ checklist_script_gen.sh
@@ -10,9 +10,10 @@ generate_status_script() {
   then
     # only check geonames_id without prior 200 status code
     cat checklist_status.tsv > checklist_status_previous.tsv
-    cat checklist_status.tsv | tail -n +2 | grep -v "200$" | cut -f1 | sort -n > geonames_id_non_200.tsv
-    cat wkt_string.tsv | sort -n > wkt_string_sorted.tsv
-    join --nocheck-order wkt_string_sorted.tsv geonames_id_non_200.tsv -t $'\t'> wkt_string_to_be_checked.tsv 
+    cat checklist_status.tsv | tail -n +2 | grep "200$" | cut -f1 | sort -n > ids_available.tsv
+    cat wkt_string.tsv | cut -f1 | sort -n | uniq > ids_all.tsv
+    diff --changed-group-format='%<' --unchanged-group-format='' ids_all.tsv ids_available.tsv | sed 's/^/\^/' > ids_to_be_checked.tsv
+    cat wkt_string.tsv | grep -f ids_to_be_checked.tsv > wkt_string_to_be_checked.tsv 
     cat checklist_status_previous.tsv | head -n1 > checklist_status.tsv
     cat checklist_status_previous.tsv | tail -n +2 | grep "200$" >> checklist_status.tsv
   else
